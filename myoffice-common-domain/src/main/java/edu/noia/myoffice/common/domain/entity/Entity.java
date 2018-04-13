ab178d8e9d949bd360c695b4a21546d12883d381
vondacho <olivier.vondach@obya.ch>
2018-04-13 07:47:43 +0200

    Javadoc

--- myoffice-common-domain/src/main/java/edu/noia/myoffice/common/domain/entity/Entity.java
+++ myoffice-common-domain/src/main/java/edu/noia/myoffice/common/domain/entity/Entity.java
@@ -7,25 +7,78 @@ import edu.noia.myoffice.common.domain.vo.Identity;
 
 import java.util.List;
 
+/**
+ * This interface specifies the minimal api of a domain entity, basically composed of an identity and a state.
+ *
+ * @param <E> Any class that implements {@link Entity}
+ * @param <I> Any class that implements {@link Identity}
+ * @param <S> Any class that implements {@link EntityState}
+ */
 public interface Entity<E extends Entity<E, I, S>, I extends Identity, S extends EntityState> {
 
+    /**
+     * @return the identity of the entity
+     */
     I getId();
 
+    /**
+     * @return a snapshot of the current state hosted by the entity
+     */
     S getState();
 
+    /**
+     * Replaces the attributes values of the entity state by the ones contained inside the given modifier state
+     *
+     * @param modifier instance of {@link EntityState}
+     * @return the entity instance
+     */
     E modify(S modifier);
 
+    /**
+     * Replaces the attributes values of the entity state by the non-null ones contained inside the given modifier state
+     *
+     * @param modifier instance of {@link EntityState}
+     * @return the entity instance
+     */
     E patch(S modifier);
 
+    /**
+     * Validates the attributes values of the given entity state
+     *
+     * @param state instance of {@link EntityState}
+     */
+    void validate(S state);
+
+    /**
+     * Saves the entity state using the given entity repository
+     *
+     * @param repository instance of {@link EntityRepository} dedicated to the implementing entity class
+     * @return the entity instance
+     */
     E save(EntityRepository<E, I, S> repository);
 
+    /**
+     * Saves the entity state using the given entity repository and publish the domain events emitted
+     * when invoking the entity methods
+     *
+     * @param repository instance of {@link EntityRepository} dedicated to the implementing entity class
+     * @param publisher  instance of {@link EventPublisher} responsible for publishing the domain events
+     * @return the entity instance
+     */
     default E save(EntityRepository<E, I, S> repository, EventPublisher publisher) {
         return save(repository).publishEvents(publisher);
     }
 
+    /**
+     * @return the list of domain {@link Event} instances emitted and accumulated when invoking the entity methods
+     */
     List<Event> getRegisteredEvents();
 
+    /**
+     * Publish the domain {@link Event} instances emitted and accumulated when invoking the entity methods
+     *
+     * @param publisher instance of {@link EventPublisher}
+     * @return the entity instance
+     */
     E publishEvents(EventPublisher publisher);
-
-    void validate(S state);
 }
--- myoffice-common-domain/src/main/java/edu/noia/myoffice/common/domain/entity/EntityState.java
+++ myoffice-common-domain/src/main/java/edu/noia/myoffice/common/domain/entity/EntityState.java
@@ -1,11 +1,28 @@
 package edu.noia.myoffice.common.domain.entity;
 
+/**
+ * This interface specifies the minimal api of a domain entity state, it basically provide modifier methods.
+ */
 public interface EntityState {
 
+    /**
+     * Replaces the internal attributes values by the ones contained inside the given modifier state.
+     * The default behavior does nothing.
+     *
+     * @param modifier instance of {@link EntityState}
+     * @return the current updated instance
+     */
     default EntityState modify(EntityState modifier) {
         return this;
     }
 
+    /**
+     * Replaces the internal attributes values by the non-null ones contained inside the given modifier state.
+     * The default behavior does nothing.
+     *
+     * @param modifier instance of {@link EntityState}
+     * @return the current updated instance
+     */
     default EntityState patch(EntityState modifier) {
         return this;
     }
